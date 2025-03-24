9655e051ec2a2459579d0d6c358f52eb4b7933f7
git12nazneen <lipinazneen65@gmail.com>
2025-03-25 00:47:15 +0600

    portfolio

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <section className="relative py-20 md:py-28 lg:py-36 overflow-hidden h-screen flex items-center justify-center">
+      <h1 className="text-center text-lg md:text-2xl font-semibold">
+        Thanks for visiting.. this site will be updated soon
+      </h1>
+    </section>
+  )
+}
+
+export default Page
