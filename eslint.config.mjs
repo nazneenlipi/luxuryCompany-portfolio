8aac83404ec948e494676f4262f6c2f7f975feca
git12nazneen <lipinazneen65@gmail.com>
2025-03-25 02:23:08 +0600

    config change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      // Add more rules here:
+      'no-unused-vars': 'off', // Example: Disable no-unused-vars
+      'no-console': 'off', // Example: Disable no-console
+      '@typescript-eslint/no-explicit-any': 'off', // Example: Disable no explicit any
+      'some-other-rule': 'off', // Example: disable some other rule.
+    },
+  },
 ];
 
 export default eslintConfig;
